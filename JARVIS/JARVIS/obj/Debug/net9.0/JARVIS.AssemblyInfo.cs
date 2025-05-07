11aeff0a0e456096a33227858e952642cfc01224
DrewnRose0133 <deviouska24de@gmail.com>
2025-05-07 15:35:37 -0400

    initial

--- JARVIS/JARVIS/obj/Debug/net9.0/JARVIS.AssemblyInfo.cs
+++ JARVIS/JARVIS/obj/Debug/net9.0/JARVIS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JARVIS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b96fae65c24c980cee441039c958209c5e6a71f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e763a61b68d4483ba9b144b80695851f6d0dd6c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("JARVIS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JARVIS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
